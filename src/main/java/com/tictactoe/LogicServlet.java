f744fde364e4c5941c7997b6451fb9df254b6472
Лёха <alex-himki@mail.ru>
2024-01-15 19:42:33 +0300

    Finally

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,108 @@
+package com.tictactoe;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Получаем текущую сессию
+        HttpSession currentSession = request.getSession();
+
+        // Получаем объект игрового поля из сессии
+        Field field = extractField(currentSession);
+
+        // получаем индекс ячейки, по которой произошел клик
+        int index = getSelectedIndex(request);
+        Sign currentSign = field.getField().get(index);
+
+        // Проверяем, что ячейка, по которой был клик пустая.
+        // Иначе ничего не делаем и отправляем пользователя на ту же страницу без изменений
+        // параметров в сессии
+        if (Sign.EMPTY != currentSign) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(request, response);
+            return;
+        }
+
+        // ставим крестик в ячейке, по которой кликнул пользователь
+        field.getField().put(index, Sign.CROSS);
+
+        // Получаем пустую ячейку поля и не победил ли нолик после добавление последнего нолика
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+        if (emptyFieldIndex >= 0) {
+            field.getField().put(emptyFieldIndex, Sign.NOUGHT);
+            if(checkWin(response, currentSession,field)) {
+                return;
+            } else {
+                currentSession.setAttribute("draw", true);
+            }
+        }
+
+        // Считаем список значков
+        List<Sign> data = field.getFieldData();
+
+        // Обновляем объект поля и список значков в сессии
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", field);
+        response.sendRedirect("/index.jsp");
+
+        // Проверяем, не победил ли крестик после добавления последнего клика пользователя
+        if (checkWin(response, currentSession, field)) {
+            return;
+        }
+
+    }
+
+    // Метод проверяет, нет ли трех крестиков/ноликов в ряд.
+    // Возвращает true/false
+    private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
+        Sign winner = field.checkWin();
+        if (Sign.CROSS == winner || Sign.NOUGHT == winner) {
+
+            // Добавляем флаг, который показывает что кто-то победил
+            currentSession.setAttribute("winner", winner);
+
+            // Считаем список значков
+            List<Sign> data = field.getFieldData();
+
+            // Обновляем этот список в сессии
+            currentSession.setAttribute("data", data);
+
+            // Шлем редирект
+            response.sendRedirect("/index.jsp");
+            return true;
+        }
+        return false;
+    }
+
+
+    private Field extractField(HttpSession currentSession) {
+        Object fieldAttribute = currentSession.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            currentSession.invalidate();
+            throw new RuntimeException("Session is broken, try one more time");
+        }
+        return (Field) fieldAttribute;
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/tictactoe/RestartServlet.java
@@ -0,0 +1,25 @@
+package com.tictactoe;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "RestartServlet", value = "/restart")
+public class RestartServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("/start");
+    }
+}
